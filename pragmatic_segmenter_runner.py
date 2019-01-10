3e079a811ef9a5e8ae7b222d289c7cce0499465f
Lucas Spreng <lucasspreng@gmail.com]>
2019-01-10 20:44:19 -0200

    Novas coisas em Python e Ruby

---
+++ pragmatic_segmenter_runner.py
@@ -0,0 +1,32 @@
+import os
+listOfFiles = list()
+for (dirpath, dirnamonth, filenamonth) in os.walk("headers"):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenamonth]
+
+for file in listOfFiles:
+    if file.endswith(".txt"):
+        caminho = file.split('/')
+        caminho = caminho[caminho.__len__()-1]
+        caminho = caminho.replace("..", ".")
+
+        sendToPragmatic = ''
+
+        with open(file) as f:
+            lines = [line.rstrip('\n') for line in open(file)]
+            head, sep, tail = lines[0].partition("FN ")
+            fonte = tail.replace(" ", "_").lower()
+
+            for l in lines:
+                l.replace("\ufeff", "")
+
+                if l.startswith("TTL|"):
+                    sendToPragmatic = l[4:] + '\n'
+
+                if l.startswith("CON|"):
+                    sendToPragmatic += " "
+                    sendToPragmatic += l[4:]
+
+            with open("pragmatic/cache/" + caminho[:-3]+'seg', "w+") as fileReady:
+                fileReady.write(sendToPragmatic)
+
+            pragmaticReturn = os.system('ruby ps.rb "' + caminho[:-3]+'seg' + '"')
---
+++ ps.rb
@@ -0,0 +1,27 @@
+require 'pragmatic_segmenter'
+
+fileLocation = ARGV[0]
+content = ""
+f = File.open("pragmatic/cache/"+fileLocation, "r")
+f.each_line do |line|
+	if content == ""
+		content += line
+	else
+		content += line
+	end
+end
+f.close
+
+ps = PragmaticSegmenter::Segmenter.new(text: content)
+
+segments = ps.segment
+
+stringFinal = ""
+segments.each do |seg|
+	stringFinal += seg + "\n"
+end
+
+
+f = File.open("pragmatic/"+fileLocation, "w+")
+f.write(stringFinal)
+f.close

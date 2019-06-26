b553947157e0c48d3321c630def87f544bbca321
Rafael <rafaelruggi@gmail.com>
2019-06-26 18:47:52 -0300

    inclusion of VRT files with XML constructors.

--- abstract_normalizer/vrt_normalizer.py
+++ abstract_normalizer/vrt_normalizer.py
@@ -7,11 +7,28 @@ import xml.etree.cElementTree as ET
 
 import dictionaries
 
+# function to indent the XML properly
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
+
 
 listOfFiles = list()
 for (dirpath, dirnamonth, filenamonth) in os.walk("../stanford"):
     listOfFiles += [os.path.join(dirpath, file) for file in filenamonth]
 
+lines = []
 
 for file in listOfFiles:
     if file.endswith(".stf"):
@@ -38,21 +55,27 @@ for file in listOfFiles:
                 journalName = line[4:].rstrip().replace("-"," ").upper()
                 field = dictionaries.dictionaryFields()[journalName]
                 discipline = dictionaries.dictionaryDisciplines()[field]
-        
   
-        with open(file) as json_file:
-
-            data = json.load(json_file)
-            for sentence in data['sentences']:
-                for word in sentence['tokens']:
-                    print (word['word'])
-
 
         #inputing metadata text tags on file.
-        text = ET.Element('text', abstrct_name=abstractName,  jounal_name=journalName, field=field, discipline=discipline)
-
-    
+        text = ET.Element('text', abstrct_name=abstractName,  jounal_name=journalName, field=field, discipline=discipline)      
+        
         #dealing with the stanfordCoreNLP sstricture and files.
+        with open(file) as json_file:
+            data = json.load(json_file)
+            # tag <p>
+            p = ET.SubElement(text, 'p')
+            for sentence in data['sentences']:
 
+                for token in sentence['tokens']:
+                    lines.append(token['word'] + "\t" + token['pos'] + "\t" + token['lemma'] + "\t" + token['originalText'])
+                # tag <s>
+                s = ET.SubElement(p, 's').text = "\n"+"\n".join(lines)+"\n"
+                lines = []
+
+        
+        indent(text)
+        
         tree = ET.ElementTree(text)
+
         tree.write("../vrt/" + fileNameNoExt+".vrt",  encoding="utf8", xml_declaration=True, method="xml")

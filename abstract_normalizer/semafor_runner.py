a4aa8b45fc6f6967cd4c7b53975ce0ecbd334790
NichZilli <nich.zilli@hotmail.com>
2020-04-30 10:04:57 -0300

    semafor runner

---
+++ abstract_normalizer/semafor_runner.py
@@ -0,0 +1,22 @@
+import os
+import multiprocessing
+
+def run():
+	os.chdir('../pragmatic')
+	list_of_files = list()
+	for (dirpath, dirname, filename) in os.walk("../pragmatic"):
+		list_of_files += [file for file in filename]
+
+	for file in list_of_files:
+		if file.endswith(".seg"):
+			file_name = os.path.splitext(file)[0]
+			command = './home/zilli/semafor/bin/runSemafor.sh' + str('../pragmatic/'+file + ' ../semafor_output/new_run/'+file_name + '.sem' + ' 1')
+			os.system(command)
+
+p1 = multiprocessing.Process(target=run)
+
+p1.start()
+p1.join()
+p1.terminate()
+
+#Obs: The directories needs to be changed, according where was installed the semafor and prose_hen folders
